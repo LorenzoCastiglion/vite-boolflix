d850440de3f5f791686c91b0b5f18045f86f382f
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-11-16 16:28:44 +0100

    nav bar ricerca

--- src/components/SearchBarComp.vue
+++ src/components/SearchBarComp.vue
@@ -1,23 +1,30 @@
 <template>
-    <div>
-        <form>
-            <div class="d-flex justify-content-end">
 
-                <div class="col-sm-3 my-1">
-                    <label class="sr-only" for="inlineFormInputGroupUsername">Username</label>
-                    <div class="input-group">
 
-                        <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="search">
-                    </div>
-                </div>
+    <div class="d-flex justify-content-between p-4 px-5 align-items-center blur">
 
-                <div class="col-auto my-1">
-                    <button type="submit" class="btn btn-primary">Find</button>
+        <div>
+            <img src="../../public/img/BOOLFLIX.png" alt="">
+        </div>
+
+
+        <div class="col-4 d-flex ">
+            <div class="col my-1">
+
+                <div class="input-group">
+                    <input type="text" class="form-control text-danger bg-transparent" id="serchMedia"
+                        placeholder="search">
                 </div>
             </div>
-        </form>
+            <div class=" my-1">
+                <button type="submit" class="btn"> <i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+        </div>
 
     </div>
+
+
+
 </template>
 
 <script>
@@ -29,5 +36,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.blur {
+    backdrop-filter: blur(10px);
+}
 
+img {
+    width: 200px;
+
+}
+
+i {
+    color: #CE1000;
+}
 </style>
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { reactive } from "vue";
+
+
+export const store = reactive({
+    apiUrl: 'https://api.themoviedb.org/3/',
+    api_key: '17f37c920e1c0e0a5f05cac02361fe4c',
+    query: ''
+
+});

dacf25360eac881e955d04652c5f30118d8af5ce
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-11-16 14:58:58 +0100

    scaffholding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <SearchBarComp />
+  </div>
+
+</template>
+
+<script>
+import SearchBarComp from './components/SearchBarComp.vue';
+
+export default {
+  components: {
+    SearchBarComp,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/SearchBarComp.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <form>
+            <div class="d-flex justify-content-end">
+
+                <div class="col-sm-3 my-1">
+                    <label class="sr-only" for="inlineFormInputGroupUsername">Username</label>
+                    <div class="input-group">
+
+                        <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="search">
+                    </div>
+                </div>
+
+                <div class="col-auto my-1">
+                    <button type="submit" class="btn btn-primary">Find</button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'SearchBarComp',
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>

abaf23ae1d541db2c9d9aac776052615412dc446
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-11-16 17:17:39 +0100

    test api

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,33 @@
 <script>
 import SearchBarComp from './components/SearchBarComp.vue';
 
+import { store } from './store';
+import axios from 'axios';
+
 export default {
+  data() {
+    return {
+      store,
+      endPointMovie: '/search/movie',
+    }
+  },
+
+  methods: {
+    getApi() {
+      const urlMovie = store.apiUrl + this.endPointMovie;
+      axios.get(urlMovie, store.options).then((res) => {
+        store.listMovie = [...res.data.results];
+        console.log(res.data.results)
+      }
+      )
+    }
+
+  },
+
+  created() {
+    this.getApi();
+  },
+
   components: {
     SearchBarComp,
   }
